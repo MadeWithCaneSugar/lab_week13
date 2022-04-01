964b0fed5c07bee23bf6f36765a0e28cb90dd140
mitchel webb <heyitsmemitchel@gmail.com>
2022-04-01 13:17:19 -0700

    adding remove cloned items function

--- src/app.js
+++ src/app.js
@@ -2,20 +2,29 @@
 const clickableDiv = document.querySelectorAll('.original-container')
 const areaCopyTo = document.querySelectorAll('.copy.container')
 
-elementCloner (clickedElement); {
-    var cloneOfElement = document.querySelector(clickedElement)
-    cloneOfElement = cloneOfElement.cloneNode(true);
+// elementCloner (clickedElement); {
+//     var cloneOfElement = document.querySelector(clickedElement)
+//     cloneOfElement = cloneOfElement.cloneNode(true);
 
-    insertAdjacentHTML
-}
+//     insertAdjacentHTML
+// }
 
-elementCloner()
+// elementCloner()
 
 //Clone w/ Morteza
 const eventDiv = document.querySelectorAll('.original-container div')
 const copyArea = document.querySelector('.copy-container')
 
 eventDiv.forEach(element => element.addEventListener('click', (evt) => {
+    console.log(evt)
     let cloneMaterial = evt.target.parentElement.outerHTML;
     copyArea.insertAdjacentHTML ("beforeend", cloneMaterial);
 }))
+
+//Remove w/ Morteza
+const resetButton = document.querySelector('button')
+
+resetButton.addEventListener('click', (evt) => {
+    console.log(evt)
+    copyArea.innerHTML = '';
+})
